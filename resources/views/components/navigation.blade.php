b24eeab87d7095e601ac214aafff32153bbe6326
ShurGith <75266693+ShurGith@users.noreply.github.com>
2025-02-27 03:55:47 +0100

    dsa

--- resources/views/components/navigation.blade.php
+++ resources/views/components/navigation.blade.php
@@ -33,6 +33,22 @@
         </div>
         <div class="hidden md:block">
           <div class="ml-4 flex items-center md:ml-6">
+            @php
+              $myc= request()->cookie('favorites');
+              if($myc)
+                 $favos = substr_count($myc, '"')/2;
+              else
+               $favos=false;
+            @endphp
+            @if($favos)
+            <div><p class="text-gray-300 text-sm">({{$favos}})</p>
+            <svg class="size-6 shrink-0 text-gray-300 {{ $favos ?? 'hidden'}} " fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                 stroke="currentColor"
+                 aria-hidden="true" data-slot="icon">
+              <path stroke-linecap="round" stroke-linejoin="round"
+                    d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z"/>
+            </svg>
+            </div>
             <button type="button"
                     class="relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
               <span class="absolute -inset-1.5"></span>
@@ -43,7 +59,7 @@
                       d="M14.857 17.082a23.848 23.848 0 0 0 5.454-1.31A8.967 8.967 0 0 1 18 9.75V9A6 6 0 0 0 6 9v.75a8.967 8.967 0 0 1-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 0 1-5.714 0m5.714 0a3 3 0 1 1-5.714 0"/>
               </svg>
             </button>
-            
+            @endif
             <!-- Profile dropdown -->
             @auth
               <div class="relative ml-3">
