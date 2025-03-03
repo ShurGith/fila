05db96453533519d305a300dce40137c93ce3990
ShurGith <75266693+ShurGith@users.noreply.github.com>
2025-03-03 19:05:35 +0100

    artes

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,7 +1,7 @@
 <x-layouts.app :meta-title="'Home'" :header-text="'Home'">
-  <div class="">
-    <h3 class="mb-4 text-xl">{{ __('Last Products') }} dewdew</h3>
-    <x-products-list :products="$products" :title="$title"/>
-  </div>
+    <div class="">
+        <h3 class="mb-4 text-xl">{{ __('Last Products') }}</h3>
+        <x-products-list :products="$products" :title="$title"/>
+    </div>
 
 </x-layouts.app>
--- resources/views/product/index.blade.php
+++ resources/views/product/index.blade.php
@@ -1,5 +1,6 @@
-<x-layouts.app :meta-title="'Listado de Productos'" :header-text="'Listado de Productos'">
-  
-  <x-products-list :products="$products"/>
+<x-layouts.app :meta-title="isset($title )? $title . ' - ' .config('app.name') : config('app.name')"
+               :header-text="isset($title) ? $title . ' - ' .config('app.name') : config('app.name')">
+
+    <x-products-list :products="$products"/>
 
 </x-layouts.app>
